e71ff348a0acf7b4560983ec762826b702c4ae7d
ShinDSR <danang.wibisono.ft20@mail.umy.ac.id>
2023-07-05 03:10:59 +0700

    add poli view

---
+++ lib/view/poli/poli.dart
@@ -0,0 +1,129 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+import '../../controller/poli_controller.dart';
+
+class Poli extends StatefulWidget {
+  const Poli({super.key});
+
+  @override
+  State<Poli> createState() => _PoliState();
+}
+
+class _PoliState extends State<Poli> {
+  var pc = PoliController();
+
+  @override
+  void initState() {
+    pc.getPoli();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('List Poli'),
+        backgroundColor: Colors.blueGrey,
+      ),
+      body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+              colors: [
+                Color(0xFF000000),
+                Color(0xFF1A237E),
+                Color(0xFF673AB7),
+              ],
+            ),
+          ),
+          child: SafeArea(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Expanded(
+                  child: StreamBuilder<List<DocumentSnapshot>>(
+                    stream: pc.stream,
+                    builder: (context, snapshot) {
+                      if (!snapshot.hasData) {
+                        return const Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      }
+
+                      final List<DocumentSnapshot> data = snapshot.data!;
+
+                      return ListView.builder(
+                        itemCount: data.length,
+                        itemBuilder: (context, index) {
+                          return Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: InkWell(
+                              child: Card(
+                                elevation: 8,
+                                child: ListTile(
+                                  leading: CircleAvatar(
+                                    backgroundColor: Colors.blue,
+                                    child: Text(
+                                      data[index]['poliname']
+                                          .substring(0, 1)
+                                          .toUpperCase(),
+                                      style: const TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                  ),
+                                  title: Text(data[index]['poliname']),
+                                  trailing: Row(
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: [
+                                      IconButton(
+                                        icon: const Icon(Icons.edit),
+                                        onPressed: () {
+                                          // Navigator.push(
+                                          //   context,
+                                          //   MaterialPageRoute(
+                                          //     builder: (context) => EditPoli(
+                                          //       id: data[index]['id'],
+                                          //       poliname: data[index]
+                                          //           ['poliname'],
+                                          //     ),
+                                          //   ),
+                                          // );
+                                        },
+                                      ),
+                                      IconButton(
+                                        icon: const Icon(Icons.delete),
+                                        onPressed: () {
+                                          pc.deletePoli(data[index].id);
+                                          pc.getPoli();
+                                        },
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ),
+                          );
+                        },
+                      );
+                    },
+                  ),
+                )
+              ],
+            ),
+          )),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Navigator.push(
+          //     context,
+          //     MaterialPageRoute(
+          //       builder: (context) => const AddPoli(),
+          //     ));
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
